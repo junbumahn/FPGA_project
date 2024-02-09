3f6009679a669abc485be97c5915a9bdc0c91a62
junbumahn <anjb215@gmail.com>
2024-02-09 15:11:28 +0900

    update encoder

--- HDMI_TX/TMDS_encoder/TDMS_encoder_tb.v
+++ HDMI_TX/TMDS_encoder/TDMS_encoder_tb.v
@@ -1,38 +1,38 @@
 `timescale 1ns/1ps
 module tb_encoder ();
 
-reg pixclk;             
-reg reset;              
-reg DE;         
-reg [7:0]data;          
-reg [1:0]ctrl;           
+reg i_pixclk;             
+reg i_reset;              
+reg i_de;         
+reg [7:0]i_data;          
+reg [1:0]i_ctrl;           
 
-wire [9:0] q_out;
+wire [9:0] o_encode;
 
 always
-#5 pixclk = ~pixclk;
+#5 i_pixclk = ~i_pixclk;
 
 initial begin
-    pixclk = 1;
+    i_pixclk = 1;
 
-    data = 8'b01010111;
-    ctrl = 2'b10;
+    i_data = 8'b01010111;
+    i_ctrl = 2'b10;
 
-    reset = 0;
+    i_reset = 0;
     #100 
-    reset = 1;
+    i_reset = 1;
     #20 
-    reset = 0;
+    i_reset = 0;
 
 end
 
 tmds_encoder
 u_tmds_encoder(
-    .pixclk(pixclk),
-    .reset(reset),
-    .DE(DE),
-    .data(data),
-    .ctrl(ctrl),
+    .i_pixclk(i_pixclk),
+    .i_reset(i_reset),
+    .i_de(i_de),
+    .i_data(i_data),
+    .i_ctrl(i_ctrl),
     .q_out(q_out)
 );
     
--- HDMI_TX/TMDS_encoder/TMDS_encoder.v
+++ HDMI_TX/TMDS_encoder/TMDS_encoder.v
@@ -2,114 +2,129 @@ module tmds_encoder (
     input i_pixclk,
     input i_reset,
 
-    input i_DE, //data enable
-    input [7:0] i_data, //8bit pixel data 
-    input [1:0] i_ctrl, //control data (Vsync,Hsync)
+    input i_de, //i_data enable
+    input [7:0] i_data, //8bit pixel i_data 
+    input [1:0] i_ctrl, //control i_data (Vsync,Hsync)
 
     output reg [9:0] o_encode //output of encoder
 );
     
-    wire [3:0] ones; // # of ones in data 
-    wire [3:0] zeroes;// # of zeroes in datreg
+    wire [3:0] ones; // # of ones in i_data 
 
-    reg [3:0] cnt; //disparity (LHS cnt == new value) (RHS cnt == old value)
+// combinational circut  calculating N1{D} and N0{D}
+    assign ones = i_data[7]+i_data[6]+i_data[5]+i_data[4]+i_data[3]+i_data[2]+i_data[1]+i_data[0];
+   
+// sequential block: stage 1 pipeline
 
+    reg [3:0] ones_pipe_1;
+    reg [7:0] data_pipe_1;
+    reg de_pipe_1;
+    reg [1:0] ctrl_pipe_1;  
+
+    always @(posedge i_pixclk) begin
+        ones_pipe_1 <= ones; //FF for stoirng "ones" vlaue
+        data_pipe_1 <= i_data;  // data buffer
+        de_pipe_1 <= i_de;  // DE buffer 1
+        ctrl_pipe_1 <= i_ctrl;  // ctrl buffer 1     
+    end
+
+// combinational circut calculating q_m, N1{q_m}, and n0{q_m}
+
+    reg [8:0] q_m;
     wire [3:0] q_m_ones;
     wire [3:0] q_m_zeroes;
 
-    reg [8:0] q_m;
-
-    assign  ones = data[7]+data[6]+data[5]+data[4]+data[3]+data[2]+data[1]+data[0];
-    assign  zeroes = 4'd8 - ones;
-    assign q_m_ones = q_m[7]+q_m[6]+q_m[5]+q_m[4]+q_m[3]+q_m[2]+q_m[1]+q_m[0];
-    assign q_m_zeroes = 4'd8 - q_m_ones;
-
     always @(*)begin
 
-        if((ones > 4) || (ones == 4 && data[0] == 1'b0)) begin
-                    q_m[0] = data[0];
-                    q_m[1] = q_m[0] ~^ data[1];
-                    q_m[2] = q_m[1] ~^ data[2];
-                    q_m[3] = q_m[2] ~^ data[3];
-                    q_m[4] = q_m[3] ~^ data[4];
-                    q_m[5] = q_m[4] ~^ data[5];
-                    q_m[6] = q_m[5] ~^ data[6];
-                    q_m[7] = q_m[6] ~^ data[7];
+        if((ones_pipe_1 > 4) || (ones_pipe_1 == 4 && data_pipe_1[0] == 1'b0)) begin
+                    q_m[0] = data_pipe_1[0];
+                    q_m[1] = q_m[0] ~^ data_pipe_1[1];
+                    q_m[2] = q_m[1] ~^ data_pipe_1[2];
+                    q_m[3] = q_m[2] ~^ data_pipe_1[3];
+                    q_m[4] = q_m[3] ~^ data_pipe_1[4];
+                    q_m[5] = q_m[4] ~^ data_pipe_1[5];
+                    q_m[6] = q_m[5] ~^ data_pipe_1[6];
+                    q_m[7] = q_m[6] ~^ data_pipe_1[7];
                     q_m[8] = 0;
                 end else begin
-                    q_m[0] = data[0];
-                    q_m[1] = q_m[0] ^ data[1];
-                    q_m[2] = q_m[1] ^ data[2];
-                    q_m[3] = q_m[2] ^ data[3];
-                    q_m[4] = q_m[3] ^ data[4];
-                    q_m[5] = q_m[4] ^ data[5];
-                    q_m[6] = q_m[5] ^ data[6];
-                    q_m[7] = q_m[6] ^ data[7];
+                    q_m[0] = data_pipe_1[0];
+                    q_m[1] = q_m[0] ^ data_pipe_1[1];
+                    q_m[2] = q_m[1] ^ data_pipe_1[2];
+                    q_m[3] = q_m[2] ^ data_pipe_1[3];
+                    q_m[4] = q_m[3] ^ data_pipe_1[4];
+                    q_m[5] = q_m[4] ^ data_pipe_1[5];
+                    q_m[6] = q_m[5] ^ data_pipe_1[6];
+                    q_m[7] = q_m[6] ^ data_pipe_1[7];
                     q_m[8] = 1;
                 end 
     end
 
-    reg [8:0] q_m_reg;
-    reg [3:0] q_m_ones_reg;
-    reg [3:0] q_m_zeroes_reg;
-    reg DE_reg;
-    reg [1:0] ctrl_reg;
+    assign q_m_ones = q_m[7]+q_m[6]+q_m[5]+q_m[4]+q_m[3]+q_m[2]+q_m[1]+q_m[0];
+    assign q_m_zeroes = 4'd8 - q_m_ones;
+
+//sequential block: stage 2 pipeline
+    reg [8:0] q_m_pipe_2;
+    reg [3:0] q_m_ones_pipe_2;
+    reg [3:0] q_m_zeroes_pipe_2;
+    reg de_pipe_2;
+    reg [1:0] ctrl_pipe_2;
     
-    
-    //pipeline
-    always @ (posedge pixclk, posedge reset) begin
-        if(reset) begin
-            q_m_reg <= 0;
-            q_m_ones_reg <= 0;
-            q_m_zeroes_reg <= 0;
-           
-        end else begin
-            q_m_reg <= q_m;
-            q_m_ones_reg <= q_m_ones;
-            q_m_zeroes_reg <= q_m_zeroes;
-            DE_reg <= DE;
-            ctrl_reg <= ctrl;
-        end
+    always @ (posedge i_pixclk) begin
+        q_m_pipe_2 <= q_m;
+        q_m_ones_pipe_2 <= q_m_ones;
+        q_m_zeroes_pipe_2 <= q_m_zeroes;
+        de_pipe_2 <= de_pipe_1;
+        ctrl_pipe_2 <= ctrl_pipe_1;
     end
 
-// D.C balanced coding: the number of "0" bits is always equal to that of "1" in a data stream
+//combinational circut calculating q_out and cnt(t)
+    reg [3:0] cnt_old ; // cnt(t-1)
+    reg [3:0] cnt ; // cnt(t)
+    reg [9:0] q_out; // temp reg for encode output
 
-    always @ (posedge pixclk, posedge reset) begin
-    if(reset) begin
-        o_encode <= 0;
-    end else begin
-        if(DE_reg) begin
-            if((!cnt) ||(q_m_ones_reg == q_m_zeroes_reg)) begin
-                o_encode[9] <= ~q_m_reg[8];
-                o_encode[8] <= q_m_reg[8];
-                o_encode[7:0] <= (q_m_reg[8]) ? q_m_reg[7:0]:~q_m_reg[7:0];
 
-            cnt <= (q_m_reg[8]) ? (cnt + q_m_ones_reg - q_m_zeroes_reg):(cnt + q_m_zeroes_reg - q_m_ones_reg);
+    always @ (*) begin
+        if(de_pipe_2) begin
+            if((!cnt_old) ||(q_m_ones_pipe_2 == q_m_zeroes_pipe_2)) begin
+                q_out[9] = ~q_m_pipe_2[8];
+                q_out[8] = q_m_pipe_2[8];
+                q_out[7:0] = (q_m_pipe_2[8]) ? q_m_pipe_2[7:0]:~q_m_pipe_2[7:0];
+
+                cnt = (q_m_pipe_2[8]) ? (cnt_old + (q_m_ones_pipe_2 - q_m_zeroes_pipe_2)):(cnt_old + (q_m_zeroes_pipe_2 - q_m_ones_pipe_2));
             end else begin
-                if(((cnt > 0) && (q_m_ones_reg > q_m_zeroes_reg)) || ((cnt < 0) && (q_m_zeroes_reg > q_m_ones_reg))) begin
-                    o_encode[9] <= 1'b1;
-                    o_encode[8] <= q_m_reg[8];
-                    o_encode[7:0] <= ~q_m_reg[7:0];
+                if(((cnt_old > 0) && (q_m_ones_pipe_2 > q_m_zeroes_pipe_2)) || ((cnt < 0) && (q_m_zeroes_pipe_2 > q_m_ones_pipe_2))) begin
+                    q_out[9] = 1'b1;
+                    q_out[8] = q_m_pipe_2[8];
+                    q_out[7:0] = ~q_m_pipe_2[7:0];
 
-                cnt <= cnt + {q_m_reg[8],1'b0} + (q_m_zeroes_reg - q_m_ones_reg); //multiplying value by 2 means shifting the value to left side by 1. However, since q_m_reg[8] is only 1 bit array, concatation is used instead of shift
+                cnt = cnt_old + {q_m_pipe_2[8],1'b0} + (q_m_zeroes_pipe_2 - q_m_ones_pipe_2); //multiplying value by 2 means shifting the value to left side by 1. However, since q_m_pipe_2[8] is only 1 bit array, concatation is used instead of shift
                 end else begin
-                    o_encode[9] <= 1'b0;
-                    o_encode[8] <= q_m_reg[8];
-                    o_encode[7:0] <= q_m_reg[7:0];
+                    q_out[9] = 1'b0;
+                    q_out[8] = q_m_pipe_2[8];
+                    q_out[7:0] = q_m_pipe_2[7:0];
 
-                cnt <= cnt - {~q_m_reg[8],1'b0} + (q_m_ones_reg - q_m_zeroes_reg);
+                cnt = cnt_old - {~q_m_pipe_2[8],1'b0} + (q_m_ones_pipe_2 - q_m_zeroes_pipe_2);
                 end
             end
         end else begin
-            case (ctrl_reg)
-                2'b00: o_encode <= 10'b1101010100;
-                2'b01: o_encode <= 10'b0010101010;
-                2'b10: o_encode <= 10'b1101010101;
-                2'b11: o_encode <= 10'b0010101011;
+            case (ctrl_pipe_2)
+                2'b00: q_out = 10'b1101010100;
+                2'b01: q_out = 10'b0010101010;
+                2'b10: q_out = 10'b1101010101;
+                default: q_out = 10'b0010101011;
             endcase
-            cnt <= 0;
+            cnt = 0;
         end
     end
-end
+
+//sequential block: stage 3 pipeline
+    always @ (posedge i_pixclk, posedge i_reset) begin
+        if(i_reset) begin
+            o_encode <= 0;
+        end else begin
+            cnt_old <= cnt;
+            o_encode <= q_out;
+        end
+    end
   
 endmodule
